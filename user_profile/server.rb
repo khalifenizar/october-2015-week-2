1e79d2252cd2c0f1659fde52432a27e9e5948787
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-10-28 14:39:47 -0400

    User Profile: Process basic login

--- user_profile/server.rb
+++ user_profile/server.rb
@@ -6,3 +6,18 @@ require "sinatra/reloader" if development?
 get "/login" do
   erb(:login_form)
 end
+
+post "/process_login" do
+  user = params[:username]
+  pass = params[:password]
+
+  if user == "nizar" && pass == "swordfish"
+    redirect to("/profile")
+  else
+    redirect to("/login")
+  end
+end
+
+get "/profile" do
+  "PROFILE HTML"
+end
